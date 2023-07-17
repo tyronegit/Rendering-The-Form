cb016fd7baded8c0eb0ff4c1e05b3476077814fb
T.Atkins <43272999+tyronegit@users.noreply.github.com>
2023-07-16 21:55:15 -0400

    Add files via upload

---
+++ RenderTheForm/src/main/java/com/tyrone/rendertheform/RenderTheFormApplication.java
@@ -0,0 +1,13 @@
+package com.tyrone.rendertheform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderTheFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderTheFormApplication.class, args);
+	}
+
+}
---
+++ RenderTheForm/src/test/java/com/tyrone/rendertheform/RenderTheFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyrone.rendertheform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderTheFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
